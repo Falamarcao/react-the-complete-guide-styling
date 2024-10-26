9f8dc2d0d3315d0e63559d92041086eff769c654
falamarcao <falamarcao@gmail.com>
2024-10-26 18:48:54 -0300

    Scoping CSS Rules with CSS Modules

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import logo from '../../assets/logo.png';
 
-import './Header.css';
+import classes from './Header.module.css';
 
 export default function Header() {
   return (
@@ -8,7 +8,10 @@ export default function Header() {
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       {/* Inline Style: */}
-      <p style={{ color: 'blue', textAlign: 'center' }}>
+      {/* <p style={{ color: 'blue', textAlign: 'center' }}>
+        A community of artists and art-lovers.
+      </p> */}
+      <p className={classes.paragraph}>
         A community of artists and art-lovers.
       </p>
     </header>
