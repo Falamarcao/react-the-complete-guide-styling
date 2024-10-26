01f05fad3b5ab1451d678b6405021cd8aa97c663
falamarcao <falamarcao@gmail.com>
2024-10-26 19:46:45 -0300

    Styled Components: Pseudo Selectors, Nested Rules & Media Queries

--- src/components/AuthInputs.tsx
+++ src/components/AuthInputs.tsx
@@ -50,6 +50,30 @@ const Input = styled.input<InputProps>`
   `}
 `;
 
+const Button = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #f0b322;
+  border-radius: 6px;
+  border: none;
+
+  &:hover {
+    background-color: #f0920e;
+  }
+`;
+
+const TextButton = styled.button`
+  color: #f0b322;
+  border: none;
+
+  &:hover {
+    color: #f0920e;
+  }
+`;
+
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState<string>('');
   const [enteredPassword, setEnteredPassword] = useState<string>('');
@@ -103,12 +127,12 @@ export default function AuthInputs() {
         </p>
       </ControlContainer>
       <div className="actions">
-        <button type="button" className="text-button">
+        <TextButton type="button" className="text-button">
           Create a new account
-        </button>
-        <button className="button" onClick={handleLogin}>
+        </TextButton>
+        <Button className="button" onClick={handleLogin}>
           Sign In
-        </button>
+        </Button>
       </div>
     </div>
   );
